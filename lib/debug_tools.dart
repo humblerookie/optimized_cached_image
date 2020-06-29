305ff9402e77fcdd6e8137ee330986c68f868f67
humblerookie <anvithv4@gmail.com>
2020-06-29 08:37:32 +0530

    chore(*): release 0.1.14

--- lib/debug_tools.dart
+++ lib/debug_tools.dart
@@ -1,13 +1,12 @@
-import 'package:flutter/cupertino.dart';
 import 'dart:developer' as developer;
 
-abstract class Logger{
-  static  bool enableLogging = false;
-  static const TAG="optimized_image_cache";
+abstract class Logger {
+  static bool enableLogging = false;
+  static const TAG = "optimized_image_cache";
 }
 
 void log(String message) {
   if (Logger.enableLogging) {
-    developer.log(message,name: Logger.TAG);
+    developer.log(message, name: Logger.TAG);
   }
 }
