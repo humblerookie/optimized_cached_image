d903d7491f7d76c3d8662765df5d6a85987d2b7a
anvith <anvithv4@gmail.com>
2021-03-08 11:26:02 +0530

    refactor(image): sync with latest cache manager

---
+++ lib/src/image_provider/_image_provider_io.dart
@@ -0,0 +1,159 @@
+import 'dart:async' show Future, StreamController, scheduleMicrotask;
+import 'dart:ui' as ui show Codec;
+
+import 'package:optimized_cached_image/src/cache/default_image_cache_manager.dart';
+import 'package:optimized_cached_image/src/cache/image_cache_manager.dart';
+
+import 'multi_image_stream_completer.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+
+import '../../optimized_cached_image.dart' show ImageRenderMethodForWeb;
+import 'optimized_cached_image_provider.dart' as image_provider;
+
+/// IO implementation of the CachedNetworkImageProvider; the ImageProvider to
+/// load network images using a cache.
+class OptimizedCacheImageProvider
+    extends ImageProvider<image_provider.OptimizedCacheImageProvider>
+    implements image_provider.OptimizedCacheImageProvider {
+  /// Creates an ImageProvider which loads an image from the [url], using the [scale].
+  /// When the image fails to load [errorListener] is called.
+  const OptimizedCacheImageProvider(
+    this.url, {
+    this.maxHeight,
+    this.maxWidth,
+    this.scale = 1.0,
+    this.errorListener,
+    this.headers,
+    this.cacheManager,
+    this.cacheKey,
+    //ignore: avoid_unused_constructor_parameters
+    ImageRenderMethodForWeb imageRenderMethodForWeb,
+  })  : assert(url != null),
+        assert(scale != null);
+
+  @override
+  final BaseCacheManager cacheManager;
+
+  /// Web url of the image to load
+  @override
+  final String url;
+
+  /// Cache key of the image to cache
+  @override
+  final String cacheKey;
+
+  /// Scale of the image
+  @override
+  final double scale;
+
+  /// Listener to be called when images fails to load.
+  @override
+  final image_provider.ErrorListener errorListener;
+
+  /// Set headers for the image provider, for example for authentication
+  @override
+  final Map<String, String> headers;
+
+  @override
+  final int maxHeight;
+
+  @override
+  final int maxWidth;
+
+  @override
+  Future<OptimizedCacheImageProvider> obtainKey(
+      ImageConfiguration configuration) {
+    return SynchronousFuture<OptimizedCacheImageProvider>(this);
+  }
+
+  @override
+  ImageStreamCompleter load(
+      image_provider.OptimizedCacheImageProvider key, DecoderCallback decode) {
+    final chunkEvents = StreamController<ImageChunkEvent>();
+    return MultiImageStreamCompleter(
+      codec: _loadAsync(key, chunkEvents, decode),
+      chunkEvents: chunkEvents.stream,
+      scale: key.scale,
+      informationCollector: () sync* {
+        yield DiagnosticsProperty<ImageProvider>(
+          'Image provider: $this \n Image key: $key',
+          this,
+          style: DiagnosticsTreeStyle.errorProperty,
+        );
+      },
+    );
+  }
+
+  Stream<ui.Codec> _loadAsync(
+    OptimizedCacheImageProvider key,
+    StreamController<ImageChunkEvent> chunkEvents,
+    DecoderCallback decode,
+  ) async* {
+    assert(key == this);
+    try {
+      var mngr = cacheManager ?? DefaultImageCacheManager();
+      assert(
+          mngr is OicImageCacheManager ||
+              (maxWidth == null && maxHeight == null),
+          'To resize the image with a CacheManager the '
+          'CacheManager needs to be an ImageCacheManager. maxWidth and '
+          'maxHeight will be ignored when a normal CacheManager is used.');
+
+      var stream = mngr is OicImageCacheManager
+          ? mngr.getImageFile(key.url,
+              maxHeight: maxHeight,
+              maxWidth: maxWidth,
+              withProgress: true,
+              headers: headers,
+              key: key.cacheKey)
+          : mngr.getFileStream(key.url,
+              withProgress: true, headers: headers, key: key.cacheKey);
+
+      await for (var result in stream) {
+        if (result is DownloadProgress) {
+          chunkEvents.add(ImageChunkEvent(
+            cumulativeBytesLoaded: result.downloaded,
+            expectedTotalBytes: result.totalSize,
+          ));
+        }
+        if (result is FileInfo) {
+          var file = result.file;
+          var bytes = await file.readAsBytes();
+          var decoded = await decode(bytes);
+          yield decoded;
+        }
+      }
+    } catch (e) {
+      // Depending on where the exception was thrown, the image cache may not
+      // have had a chance to track the key in the cache at all.
+      // Schedule a microtask to give the cache a chance to add the key.
+      scheduleMicrotask(() {
+        PaintingBinding.instance.imageCache.evict(key);
+      });
+
+      errorListener?.call();
+      rethrow;
+    } finally {
+      await chunkEvents.close();
+    }
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    if (other is OptimizedCacheImageProvider) {
+      return ((cacheKey ?? url) == (other.cacheKey ?? other.url)) &&
+          scale == other.scale &&
+          maxHeight == other.maxHeight &&
+          maxWidth == other.maxWidth;
+    }
+    return false;
+  }
+
+  @override
+  int get hashCode => hashValues(cacheKey ?? url, scale, maxHeight, maxWidth);
+
+  @override
+  String toString() => '$runtimeType("$url", scale: $scale)';
+}
---
+++ lib/src/image_provider/_image_provider_web.dart
@@ -0,0 +1,168 @@
+import 'dart:async';
+import 'dart:ui' as ui;
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+
+import '_load_async_web.dart';
+import 'multi_image_stream_completer.dart';
+import 'optimized_cached_image_provider.dart' as image_provider;
+import '../../optimized_cached_image.dart' show ImageRenderMethodForWeb;
+
+/// The dart:html implementation of [image_provider.OptimizedCacheImageProvider].
+class OptimizedCacheImageProvider
+    extends ImageProvider<image_provider.OptimizedCacheImageProvider>
+    implements image_provider.OptimizedCacheImageProvider {
+  /// Creates an object that fetches the image at the given URL.
+  ///
+  /// The arguments [url] and [scale] must not be null.
+  const OptimizedCacheImageProvider(
+    this.url, {
+    this.maxHeight,
+    this.maxWidth,
+    this.scale = 1.0,
+    this.errorListener,
+    this.headers,
+    this.cacheManager,
+    this.cacheKey,
+    ImageRenderMethodForWeb imageRenderMethodForWeb,
+  })  : _imageRenderMethodForWeb =
+            imageRenderMethodForWeb ?? ImageRenderMethodForWeb.HtmlImage,
+        assert(url != null),
+        assert(scale != null);
+
+  @override
+  final BaseCacheManager cacheManager;
+
+  @override
+  final String url;
+
+  @override
+  final String cacheKey;
+
+  @override
+  final double scale;
+
+  /// Listener to be called when images fails to load.
+  @override
+  final image_provider.ErrorListener errorListener;
+
+  @override
+  final Map<String, String> headers;
+
+  @override
+  final int maxHeight;
+
+  @override
+  final int maxWidth;
+
+  final ImageRenderMethodForWeb _imageRenderMethodForWeb;
+
+  @override
+  Future<OptimizedCacheImageProvider> obtainKey(
+      ImageConfiguration configuration) {
+    return SynchronousFuture<OptimizedCacheImageProvider>(this);
+  }
+
+  @override
+  ImageStreamCompleter load(
+      image_provider.OptimizedCacheImageProvider key, DecoderCallback decode) {
+    final chunkEvents = StreamController<ImageChunkEvent>();
+
+    return MultiImageStreamCompleter(
+        chunkEvents: chunkEvents.stream,
+        codec:
+            _loadAsync(key as OptimizedCacheImageProvider, chunkEvents, decode),
+        scale: key.scale,
+        informationCollector: _imageStreamInformationCollector(key));
+  }
+
+  InformationCollector _imageStreamInformationCollector(
+      image_provider.OptimizedCacheImageProvider key) {
+    InformationCollector collector;
+    assert(() {
+      collector = () {
+        return <DiagnosticsNode>[
+          DiagnosticsProperty<ImageProvider>('Image provider', this),
+          DiagnosticsProperty<OptimizedCacheImageProvider>(
+              'Image key', key as OptimizedCacheImageProvider),
+        ];
+      };
+      return true;
+    }());
+    return collector;
+  }
+
+  Stream<ui.Codec> _loadAsync(
+    OptimizedCacheImageProvider key,
+    StreamController<ImageChunkEvent> chunkEvents,
+    DecoderCallback decode,
+  ) {
+    switch (_imageRenderMethodForWeb) {
+      case ImageRenderMethodForWeb.HttpGet:
+        return _loadAsyncHttpGet(key, chunkEvents, decode);
+      case ImageRenderMethodForWeb.HtmlImage:
+        return loadAsyncHtmlImage(key, chunkEvents, decode).asStream();
+    }
+    throw UnsupportedError(
+        'ImageRenderMethod $_imageRenderMethodForWeb is not supported');
+  }
+
+  Stream<ui.Codec> _loadAsyncHttpGet(
+    OptimizedCacheImageProvider key,
+    StreamController<ImageChunkEvent> chunkEvents,
+    DecoderCallback decode,
+  ) async* {
+    assert(key == this);
+    try {
+      var mngr = cacheManager ?? DefaultCacheManager();
+      await for (var result in mngr.getFileStream(key.url,
+          withProgress: true, headers: headers)) {
+        if (result is DownloadProgress) {
+          chunkEvents.add(ImageChunkEvent(
+            cumulativeBytesLoaded: result.downloaded,
+            expectedTotalBytes: result.totalSize,
+          ));
+        }
+        if (result is FileInfo) {
+          var file = result.file;
+          var bytes = await file.readAsBytes();
+          var decoded = await decode(bytes);
+          yield decoded;
+        }
+      }
+    } catch (e) {
+      // Depending on where the exception was thrown, the image cache may not
+      // have had a chance to track the key in the cache at all.
+      // Schedule a microtask to give the cache a chance to add the key.
+      scheduleMicrotask(() {
+        PaintingBinding.instance.imageCache.evict(key);
+      });
+
+      errorListener?.call();
+      rethrow;
+    } finally {
+      await chunkEvents.close();
+    }
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+    if (other is OptimizedCacheImageProvider) {
+      var sameKey = (cacheKey ?? url) == (other.cacheKey ?? other.url);
+      return sameKey && scale == other.scale;
+    } else {
+      return false;
+    }
+  }
+
+  @override
+  int get hashCode => ui.hashValues(url, scale, cacheKey);
+
+  @override
+  String toString() => '$runtimeType("$url", scale: $scale)';
+}
