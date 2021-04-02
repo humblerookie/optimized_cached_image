b338703c63444c3ce59a129342e624c1d4ea1449
Anvith <anvithv4@gmail.com>
2021-04-02 09:05:30 +0530

    chore(nullsafety): upgrade dependencies to null safe versions
    Additionally fix issues with hero widget

--- lib/optimized_cached_image.dart
+++ lib/optimized_cached_image.dart
@@ -8,3 +8,4 @@ export 'package:flutter_cache_manager/src/result/download_progress.dart';
 export 'src/oci_widget.dart';
 export 'src/image_provider/optimized_cached_image_provider.dart';
 export 'src/image_provider/multi_image_stream_completer.dart';
+export 'src/debug_tools.dart';
