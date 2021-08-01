7805efa81be2f04954bcfd104848603ec292d286
humblerookie <1428864+humblerookie@users.noreply.github.com>
2021-08-01 09:47:32 +0530

    feat(null-safety): migrate to null safety

--- lib/src/transformer/image_transformer.dart
+++ lib/src/transformer/image_transformer.dart
@@ -31,12 +31,13 @@ class DefaultImageTransformer extends ImageTransformer {
   };
 
   @override
-  Future<FileInfo> transform(FileInfo info, int width, int height) async {
+  Future<FileInfo> transform(FileInfo info, int? width, int? height) async {
     final value = await _scaleImageFile(info, width, height);
     return value;
   }
 
-  Future<FileInfo> _scaleImageFile(FileInfo info, int width, int height) async {
+  Future<FileInfo> _scaleImageFile(
+      FileInfo info, int? width, int? height) async {
     FileInfo fileInfo = info;
     final file = fileInfo.file;
     log("Scaling file.. ${fileInfo.originalUrl}");
@@ -55,7 +56,7 @@ class DefaultImageTransformer extends ImageTransformer {
       final localFileSystem = fileIo.LocalFileSystem();
       resizedFile = localFileSystem.file(scaleInfo.file.path);
 
-      if (resizedFile != null && resizedFile.existsSync()) {
+      if (resizedFile.existsSync()) {
         if (resizedFile.lengthSync() < srcSize) {
           log("Resized success ${fileInfo.originalUrl}");
         } else {
@@ -74,7 +75,7 @@ class DefaultImageTransformer extends ImageTransformer {
   }
 
   @override
-  ScaleInfo getScaledFileInfo(File file, int width, int height) {
+  ScaleInfo getScaledFileInfo(File file, int? width, int? height) {
     final format = _getCompressionFormat(file);
 
     final directory = file.parent;
@@ -82,18 +83,18 @@ class DefaultImageTransformer extends ImageTransformer {
         "/" +
         p.basenameWithoutExtension(file.path) +
         sprintf(tmpFileSuffix, [width ?? 1, height ?? 1]) +
-        _extensionFormats[format];
+        _extensionFormats[format]!;
     final scaleFile = File(destPath);
     return ScaleInfo(scaleFile, width ?? 1, height ?? 1, format);
   }
 
   CompressFormat _getCompressionFormat(File file) {
-    String extension = p.extension(file.path) ?? '';
+    String extension = p.extension(file.path);
     return _compressionFormats[extension] ?? CompressFormat.png;
   }
 }
 
 abstract class ImageTransformer {
-  Future<FileInfo> transform(FileInfo info, int width, int height);
+  Future<FileInfo> transform(FileInfo info, int? width, int? height);
   ScaleInfo getScaledFileInfo(File file, int width, int height);
 }
